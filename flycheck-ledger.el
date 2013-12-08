2a0839699f2e26e142b24e79ad7668a58b77f971
Steve Purcell <steve@sanityinc.com>
2013-12-08 19:59:31 +0000

    Add usage hint

--- flycheck-ledger.el
+++ flycheck-ledger.el
@@ -25,6 +25,10 @@
 ;; This flychecker uses the output of "ledger balance" on the current file to
 ;; find errors such as unbalanced transactions and syntax errors.
 
+;;;; Setup
+
+;; (eval-after-load 'flycheck '(require 'flycheck-ledger))
+
 ;;; Code:
 
 (require 'flycheck)
